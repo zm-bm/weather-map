3115acd7507a724d47858c7795bfcfe3ed095d33
Eric VanderHelm <vanderhelm.eric@gmail.com>
2025-12-30 19:48:54 -0600

    feat(frontend): add temp + label layers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,31 @@ import './App.css'
 function App() {
   
   useEffect(() => {
-    // Initialize map once
     const map = L.map("map", {
       center: [39.5, -98.35],
       zoom: 4,
+      maxZoom: 7,
       zoomControl: true,
     });
 
-    // Base map (Carto light)
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("/temp/{z}/{x}/{y}.png", {
+      tms: true,
+    }).addTo(map);
+
+    // Base map layer
+    L.tileLayer(
+    "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+      opacity: 0.3,
+    }).addTo(map);
+
+    // Label layer
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png", {
       attribution: '&copy; OpenStreetMap &copy; CARTO',
       maxZoom: 19,
     }).addTo(map);
 
     return () => {
-      map.remove(); // cleanup on unmount
+      map.remove();
     };
   }, []);
 
