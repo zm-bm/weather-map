53344ef88e613b144cb9744e6c035f1d636173e4
Eric VanderHelm <vanderhelm.eric@gmail.com>
2026-06-10 15:12:57 -0500

    feat: add catalog and pipeline URIs

--- infra/weather-etl/publisher.tf
+++ infra/weather-etl/publisher.tf
@@ -65,6 +65,8 @@ resource "aws_lambda_function" "publisher" {
   environment {
     variables = {
       ARTIFACT_ROOT_URI   = local.artifact_root_uri
+      CATALOG_URI         = local.catalog_uri
+      PIPELINE_URI        = local.pipeline_uri
       PUBLISH_DATASETS    = join(",", var.publisher_datasets)
       PUBLISH_CYCLE_COUNT = tostring(var.publisher_cycle_count)
     }
